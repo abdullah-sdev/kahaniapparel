b14ccc19f11c32ca733813b248bdd576841ad0e6
Blue Spirit <bluewalkerz.programmer@gmail.com>
2025-02-22 23:20:36 +0500

    Generate & Make: CargoCompanies & Discounts migrations with extra files

---
+++ app/Http/Controllers/CargoCompanyController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CargoCompany;
+use App\Http\Requests\StoreCargoCompanyRequest;
+use App\Http\Requests\UpdateCargoCompanyRequest;
+
+class CargoCompanyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCargoCompanyRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CargoCompany $cargoCompany)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CargoCompany $cargoCompany)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCargoCompanyRequest $request, CargoCompany $cargoCompany)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CargoCompany $cargoCompany)
+    {
+        //
+    }
+}
