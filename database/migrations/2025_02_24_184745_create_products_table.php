8847ab552ea2215318db5a033f53f7193a5ddec8
Blue Spirit <bluewalkerz.programmer@gmail.com>
2025-02-25 00:15:06 +0500

    Generate & Make: Product migrations with extra files

---
+++ database/migrations/2025_02_24_184745_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('price', 10, 2);
+            $table->text('description')->nullable();
+            $table->string('thumbnail_image')->nullable();
+            $table->string('thumbnail_image1')->nullable();
+            $table->integer('clicks')->default(0);
+            $table->boolean('is_in_stock')->default(true);
+            $table->boolean('is_enable')->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
